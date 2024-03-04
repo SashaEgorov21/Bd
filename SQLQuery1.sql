6826fccc803b9d21677f7c11bde9b2d814497767
alex <sasha20021221@gmail.com>
2024-03-04 23:42:55 +0300

    bd

---
+++ SQLQuery1.sql
@@ -0,0 +1,12 @@
+Create Database pr4;
+Create Table Employees
+(
+EmployeeID int Primary key,
+FirstName varchar(50),
+LastName varchar(50),
+)
+Create table Departments
+(
+DepartmentId int primary key,
+DepartName varchar(50),
+)
